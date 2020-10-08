219cc94577142fb45798ec4bb79c67d179c36820
openlab <openlab@z00p1-17.SPSKNM>
2020-10-08 09:15:50 +0200

    Exercise 1 finished

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,5 @@
-let a: string = "Hello appslab"
-console.log(a);
+let height: number = 0.2;
+let lenght: number = 0.5;
+let tower: number = 100;
+let totalDistance: number = height*lenght*tower;
+console.log(totalDistance);
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,6 @@
-var a = "Hello appslab";
-console.log(a);
+var height = 0.2;
+var lenght = 0.5;
+var tower = 100;
+var totalDistance = height * lenght * tower;
+console.log(totalDistance);
 //# sourceMappingURL=helloAppslabTs.js.map
