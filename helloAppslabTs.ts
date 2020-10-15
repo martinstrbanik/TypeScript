af339bf58590bba9eee50d54b1cd35ef33f3a081
Martin3 <strbanik.jun@gmail.com>
2020-10-15 14:37:34 +0200

    Exercise 3 finished

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,16 @@
-let a: string = "Hello appslab"
-console.log(a);
+let a: number = 7;
+let b: number = 8;
+let c: number = 12;
+
+if (c<b||c<a)
+{
+    console.log("c has to be longest");
+}
+else if (a+b>c)
+{
+    console.log("triangle is possible");
+}
+
+else{
+    console.log("triangle is impossible");
+}
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,13 @@
-var a = "Hello appslab";
-console.log(a);
+var a = 7;
+var b = 8;
+var c = 12;
+if (c < b || c < a) {
+    console.log("c has to be longest");
+}
+else if (a + b > c) {
+    console.log("triangle is possible");
+}
+else {
+    console.log("triangle is impossible");
+}
 //# sourceMappingURL=helloAppslabTs.js.map
