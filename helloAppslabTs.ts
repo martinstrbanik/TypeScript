b8ac77b56183ab0a27e339bb77d1567bddbc27ed
Martin3 <strbanik.jun@gmail.com>
2020-10-15 14:54:38 +0200

    Exercise 4 finished

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,5 @@
-let a: string = "Hello appslab"
-console.log(a);
+let a:number[] = [10,20,15,12,35];
+
+for (let i:number=0;i<a.length;i++){
+    console.log(a[i]*a.length)
+}
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,5 @@
-var a = "Hello appslab";
-console.log(a);
+var a = [10, 20, 15, 12, 35];
+for (var i = 0; i < a.length; i++) {
+    console.log(a[i] * a.length);
+}
 //# sourceMappingURL=helloAppslabTs.js.map
