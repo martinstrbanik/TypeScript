762c888ab321c56349c1ba514b1ddbca5250f90c
Martin3 <strbanik.jun@gmail.com>
2020-10-15 14:25:23 +0200

    Finished exercise 2

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,34 @@
-let a: string = "Hello appslab"
-console.log(a);
+let a: number = 60;
+let b: number = 20;
+let c: number = 20;
+
+
+
+let d: number;
+
+if (a==b && b==c)
+        {
+            d = 3;
+        }
+
+        else if (a==c)
+        {
+            d = 2;
+        }
+        else if (a==b)
+        {
+            d = 2;
+        }
+        else if (c==b)
+        {
+            d = 2;
+        }
+        else
+        {
+            d = 0;
+        }
+       
+ console.log("Number of same numbers: " + d);              
+
+        
+
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,21 @@
-var a = "Hello appslab";
-console.log(a);
+var a = 60;
+var b = 20;
+var c = 20;
+var d;
+if (a == b && b == c) {
+    d = 3;
+}
+else if (a == c) {
+    d = 2;
+}
+else if (a == b) {
+    d = 2;
+}
+else if (c == b) {
+    d = 2;
+}
+else {
+    d = 0;
+}
+console.log("Number of same numbers: " + d);
 //# sourceMappingURL=helloAppslabTs.js.map
