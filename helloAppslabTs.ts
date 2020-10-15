be1b878e88f52edc623c43fda966340e586e9215
Martin3 <strbanik.jun@gmail.com>
2020-10-15 15:09:57 +0200

    Exercise 5 finished

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,13 @@
-let a: string = "Hello appslab"
-console.log(a);
+let totalSlices:number = 12;
+let recipients:number = 4;
+let slicePerRec:number = 3;
+
+if(recipients*slicePerRec<=totalSlices)
+        {
+            console.log("True");
+
+        }
+        else
+            {
+                console.log("False");
+            }
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,10 @@
-var a = "Hello appslab";
-console.log(a);
+var totalSlices = 12;
+var recipients = 4;
+var slicePerRec = 3;
+if (recipients * slicePerRec <= totalSlices) {
+    console.log("True");
+}
+else {
+    console.log("False");
+}
 //# sourceMappingURL=helloAppslabTs.js.map
